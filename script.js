3677caf17c21b5a72b468db26b437e1f5ad01925
Quentin ANIERE <quentin.aniere@gmail.com>
2020-11-08 11:51:46 +0100

    Finalisation du glisser/déposer

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var ligneActuelle = 10;
-//On commence à remplir la dixième puis on descend
-
-code = ["Bleu", "Blanc", "Rouge", "Vert"]
-lignes = []
+var ligneActuelle = 10; //On commence à remplir la dixième puis on descend
+var derniereCase = null //La dernière case dont un jeton a été deplacée
+var code = ["Bleu", "Blanc", "Rouge", "Vert"]
+var couleurs = ["Bleu", "Blanc", "Rouge", "Vert", "Jaune", "Orange", "Blanc", "Rose", "Violet"]
 
 function deplacement(element) {
     // Fonction qui est appellée quand on fait bouger un jeton du menu
@@ -19,22 +18,41 @@ function autoriserDeposer(element) {
 function deposer(element) {
 
     var caseReception = element.path[0].id; //id de la case qui recoit un jeton
-    var ligneJeton = parseInt(caseReception.slice(-2)); //La ligne sur laquelle on vient de poser le jeton
+    var ligneJeton = parseInt(caseReception.slice(-2)); //La ligne sur laquelle on vient de poser le jeton*
 
-    if(ligneJeton == ligneActuelle) {
+    if(ligneJeton == ligneActuelle || caseReception == "poubelle") {
 
         element.preventDefault(); //Empecher l'action par défaut
-        var jeton = element.dataTransfer.getData("text"); //On récuperer l'élement à ajouter
-        element.target.appendChild(document.getElementById(jeton)); // On l'ajoute au bon endroit
+        var jeton = element.dataTransfer.getData("text"); //On récupere l'élement à ajouter
+        element.target.appendChild(document.getElementById(jeton)); // On l'ajoute au bon 
         var destination = document.getElementById(jeton.replace("menu", ""));
+        
+        var nouveauJeton = document.createElement("div");
+        nouveauJeton.setAttribute("id", jeton);
+        nouveauJeton.setAttribute("class", "caseJeton");
+        nouveauJeton.setAttribute("draggable", "true");
+        nouveauJeton.setAttribute("ondragstart", "deplacement(event)");
+        destination.appendChild(nouveauJeton);
+        //Création d'un nouveau jeton identique à celui déplacé, puis déplacement dans le menu
 
-        var nouveauJeton = document.createElement("div")
-        nouveauJeton.setAttribute("id", jeton)
-        nouveauJeton.setAttribute("class", "caseJeton")
-        destination.appendChild(nouveauJeton)
-        //Création d'un nouveau jeton identique à celui déplacé, puis déplacement dans le mneu
+        if (origineJeton != null) {
+            origineJeton.innerHTML = "";
+        }
 
+        if (caseReception == "poubelle") {
+            document.getElementById("poubelle").innerHTML = "";
+        }
 
+        //PROCESS A APPLIQUER :
+        // 1 - Récupération du numéro de case (1 à 4) 
+        // 2 - moins 1 pour correspondre à la liste
+        // 3 - Si les 4 jetons sont entrés, vérification d'égalité avec le code
+        // 4 - Sinon, comparaison case par case et incrémantation compteur si égalité, si bonne couleur
     }
+}
 
+function seSouvenir(caseJeton) {
+    //Fonction appellée quand on retire un jeton d'une case
+    //Si la case fait partie du plateau, on note son id pour pouvoir la retrouver plus tard
+    origineJeton = !couleurs.includes(caseJeton.id) ? caseJeton : null;
 }
